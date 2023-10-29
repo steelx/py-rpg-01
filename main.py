743296fb86026a0b4f7e05aae97915ea087fa62e
Ajinkya <ajinkyax@pm.me>
2023-10-29 17:10:18 +0530

    fix: main typo

--- main.py
+++ main.py
@@ -10,43 +10,43 @@ from globals import FPS, WINDOW_SIZE, DISPLAY_SIZE
 from map_definitions import small_room_map_def
 from utils import get_faced_tile
 
+
 if __name__ == '__main__':
-    if __name__ == '__main__':
 
-        pygame.init()
-        pygame.display.set_caption("jRPG Game")
-        screen = pygame.display.set_mode(WINDOW_SIZE)
-        display = pygame.surface.Surface(DISPLAY_SIZE)
-        clock = pygame.time.Clock()
+    pygame.init()
+    pygame.display.set_caption("jRPG Game")
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    display = pygame.surface.Surface(DISPLAY_SIZE)
+    clock = pygame.time.Clock()
 
-        game = Game(display=display)
-        game.setup(small_room_map_def, ACTIONS)
+    game = Game(display=display)
+    game.setup(small_room_map_def, ACTIONS)
 
-        hero = Character(characters["hero"], game)
-        game.follow = hero.entity
+    hero = Character(characters["hero"], game)
+    game.follow = hero.entity
 
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        tile_x, tile_y = get_faced_tile(hero)
-                        trigger = game.get_trigger_at_tile(tile_x, tile_y)
-                        if trigger is not None:
-                            trigger.on_use(None, hero.entity)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    tile_x, tile_y = get_faced_tile(hero)
+                    trigger = game.get_trigger_at_tile(tile_x, tile_y)
+                    if trigger is not None:
+                        trigger.on_use(None, hero.entity)
 
-            # Game Render
-            screen.fill((0, 0, 0))
-            display.fill((0, 0, 0))
-            hero.controller.update(game.dt)
-            game.update()
-            game.render()
+        # Game Render
+        screen.fill((0, 0, 0))
+        display.fill((0, 0, 0))
+        hero.controller.update(game.dt)
+        game.update()
+        game.render()
 
-            # Scale and draw the game_surface onto the screen
-            surf = pygame.transform.scale(display, WINDOW_SIZE)
-            screen.blit(surf, (0, 0))
-            pygame.display.update()
-            game.dt = clock.tick(FPS)
+        # Scale and draw the game_surface onto the screen
+        surf = pygame.transform.scale(display, WINDOW_SIZE)
+        screen.blit(surf, (0, 0))
+        pygame.display.update()
+        game.dt = clock.tick(FPS)
