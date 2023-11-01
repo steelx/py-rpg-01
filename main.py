34f3c268ce65dc0408f07183647f7905014003ee
Ajinkya <ajinkyax@pm.me>
2023-11-01 11:08:51 +0530

    fix: pygame gui basics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+import pygame_gui
 
 from actions import ACTIONS
 from character import Character
@@ -10,14 +11,12 @@ from globals import FPS, WINDOW_SIZE, DISPLAY_SIZE
 from map_definitions import small_room_map_def
 from utils import get_faced_tile
 
-
 if __name__ == '__main__':
 
     pygame.init()
     pygame.display.set_caption("jRPG Game")
     screen = pygame.display.set_mode(WINDOW_SIZE)
     display = pygame.surface.Surface(DISPLAY_SIZE)
-    clock = pygame.time.Clock()
 
     game = Game(display=display)
     game.setup(small_room_map_def, ACTIONS)
@@ -25,7 +24,31 @@ if __name__ == '__main__':
     hero = Character(characters["hero"], game)
     game.follow = hero.entity
 
+    # UI Setup
+    ui_manager = pygame_gui.UIManager(WINDOW_SIZE, 'gui_theme.json')
+
+    panel_layout_rect = pygame.display.get_surface().get_rect().inflate(-50, -50)
+    panel = pygame_gui.elements.UIPanel(relative_rect=panel_layout_rect, manager=ui_manager)
+
+    pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(30, 20, 100, 50),
+        text='Hello 1',
+        manager=ui_manager,
+        container=panel)
+    button_rect = pygame.Rect(100, 100, 100, 50)
+    hello_button = pygame_gui.elements.UIButton(
+        relative_rect=button_rect,
+        text='Say Hello',
+        manager=ui_manager, container=panel,
+        anchors={'left': 'left',
+                 'right': 'right',
+                 'top': 'top',
+                 'bottom': 'bottom'}
+    )
+
+    clock = pygame.time.Clock()
     while True:
+        game.dt = clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -38,9 +61,16 @@ if __name__ == '__main__':
                     if trigger is not None:
                         trigger.on_use(None, hero.entity)
 
+            # Handle UI events
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == hello_button:
+                    print('Hello World!')
+            ui_manager.process_events(event)
+
         # Game Render
         screen.fill((0, 0, 0))
         display.fill((0, 0, 0))
+        ui_manager.update(game.dt)
         hero.controller.update(game.dt)
         game.update()
         game.render()
@@ -48,5 +78,5 @@ if __name__ == '__main__':
         # Scale and draw the game_surface onto the screen
         surf = pygame.transform.scale(display, WINDOW_SIZE)
         screen.blit(surf, (0, 0))
-        pygame.display.update()
-        game.dt = clock.tick(FPS)
+        ui_manager.draw_ui(screen)
+        pygame.display.flip()
